a537c27b2cd5b844eabef6b34e2bec7e3ba4c713
Austin Erwin <austinerwinus@gmail.com>
2020-09-20 12:55:11 -0500

    asdf

--- app/src/main/java/com/example/floorplan/ui/table/ActivityNewTable.java
+++ app/src/main/java/com/example/floorplan/ui/table/ActivityNewTable.java
@@ -1,9 +1,13 @@
 package com.example.floorplan.ui.table;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.floorplan.MainActivity;
 import com.example.floorplan.R;
 
 public class ActivityNewTable extends AppCompatActivity {
@@ -11,5 +15,13 @@ public class ActivityNewTable extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addtable);
+
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
     }
 }
