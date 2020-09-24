8d7a111bc9b281394760782f731e3050616509e8
Austin Erwin <austinerwinus@gmail.com>
2020-09-24 18:42:05 -0500

    retrieve inputs

--- app/src/main/java/com/example/floorplan/ui/table/ActivityNewTable.java
+++ app/src/main/java/com/example/floorplan/ui/table/ActivityNewTable.java
@@ -4,25 +4,43 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.floorplan.MainActivity;
 import com.example.floorplan.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ActivityNewTable extends AppCompatActivity {
+
+    TextInputEditText tableNumInput;
+    Button submitButton;
+
+    String tableNum;
+    Integer seatCapacity;
+    Integer length;
+    Integer width;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addtable);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        Spinner spinner = (Spinner) findViewById(R.id.seat_cap_spinner);
+        tableNumInput = (TextInputEditText) findViewById(R.id.table_num_input);
+
+
+        Spinner seatCapSpinner = (Spinner) findViewById(R.id.seat_cap_spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         List<Integer> seats = new ArrayList<Integer>();
         seats.add(1);
@@ -33,7 +51,39 @@ public class ActivityNewTable extends AppCompatActivity {
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        seatCapSpinner.setAdapter(adapter);
+
+        Spinner lengthSpinner = (Spinner) findViewById(R.id.length_spinner);
+        List<Integer> length_choices = new ArrayList<Integer>();
+        length_choices.add(1);
+        length_choices.add(2);
+        length_choices.add(3);
+        length_choices.add(4);
+        ArrayAdapter<Integer> lengthAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, length_choices);
+        lengthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        lengthSpinner.setAdapter(lengthAdapter);
+
+        Spinner widthSpinner = (Spinner) findViewById(R.id.width_spinner);
+        List<String> width_choices = new ArrayList<String>();
+        width_choices.add("1");
+        width_choices.add("2");
+        width_choices.add("3");
+        width_choices.add("4");
+        ArrayAdapter<String> widthAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, width_choices);
+        widthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        widthSpinner.setAdapter(widthAdapter);
+
+        submitButton = (Button) findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tableNum = tableNumInput.getText().toString();
+                seatCapacity = Integer.valueOf(seatCapSpinner.getSelectedItem().toString());
+                width = Integer.valueOf(widthSpinner.getSelectedItem().toString());
+                length = Integer.valueOf(lengthSpinner.getSelectedItem().toString());
+                Toast.makeText(getApplicationContext(), String.valueOf(seatCapacity), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public boolean onOptionsItemSelected(MenuItem item){
