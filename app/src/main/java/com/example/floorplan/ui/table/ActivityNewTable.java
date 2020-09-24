0eed6869a43b3e8ad54c7dac44e538b2b7dda70f
Austin Erwin <austinerwinus@gmail.com>
2020-09-24 17:46:08 -0500

    added spinner

--- app/src/main/java/com/example/floorplan/ui/table/ActivityNewTable.java
+++ app/src/main/java/com/example/floorplan/ui/table/ActivityNewTable.java
@@ -4,11 +4,16 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.floorplan.MainActivity;
 import com.example.floorplan.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActivityNewTable extends AppCompatActivity {
     @Override
@@ -17,6 +22,18 @@ public class ActivityNewTable extends AppCompatActivity {
         setContentView(R.layout.activity_addtable);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        Spinner spinner = (Spinner) findViewById(R.id.seat_cap_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        List<Integer> seats = new ArrayList<Integer>();
+        seats.add(1);
+        seats.add(2);
+        seats.add(3);
+        seats.add(4);
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, seats);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
     public boolean onOptionsItemSelected(MenuItem item){
